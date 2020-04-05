29f0959acd3f51388ba7fbe48656e2b88a7de82b
William Plunkett Jr <joey.plunkett92@yahoo.com>
2020-04-05 10:47:22 -0500

    Added New Locales

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,6 +9,7 @@ version '1.0.0'
 server_scripts {
   '@es_extended/locale.lua',
   'locales/en.lua',
+  'locales/sv.lua',
   'config.lua',
   'server/main.lua'
 }
@@ -16,6 +17,7 @@ server_scripts {
 client_scripts {
   '@es_extended/locale.lua',
   'locales/en.lua',
+  'locales/sv.lua',
   'config.lua',
   'client/main.lua'
 }
